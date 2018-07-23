a934d3d66ecf607ee538492dcef068316d4d9fdd
Andrea Toston <tostonar@gmail.com>
2018-07-23 12:25:39 -0400

    Added second cat, names for each cat

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,16 @@
 console.log('\'Allo \'Allo!');
-let clicks = 0;
-const elem = document.getElementById('my-elem');
-elem.addEventListener('click', function(){
-  clicks++;
-  document.querySelector('p').innerHTML=`You clicked ${clicks} time(s).`;
+let pic1clicks = 0;
+const elem1 = document.getElementById('my-elem1');
+const cat1Name = document.querySelector('.cat1 h2').textContent;
+elem1.addEventListener('click', function(){
+  pic1clicks++;
+  document.querySelector('#elem1-clicks').innerHTML=`You clicked ${cat1Name} ${pic1clicks} time(s).`;
+}, false);
+
+let pic2clicks = 0;
+const elem2 = document.getElementById('my-elem2');
+const cat2Name = document.querySelector('.cat2 h2').textContent;
+elem2.addEventListener('click', function(){
+  pic2clicks++;
+  document.querySelector('#elem2-clicks').innerHTML=`You clicked ${cat2Name} ${pic2clicks} time(s).`;
 }, false);
